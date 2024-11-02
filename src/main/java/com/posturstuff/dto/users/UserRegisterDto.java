4fd352ef38d13ce96d0fe1be8a0b57e6333948b2
willotavio <89710130+willotavio@users.noreply.github.com>
2024-11-02 11:55:10 -0300

    feat: add size validation in UserRegisterDto

--- src/main/java/com/posturstuff/dto/users/UserRegisterDto.java
+++ src/main/java/com/posturstuff/dto/users/UserRegisterDto.java
@@ -6,15 +6,19 @@ import java.time.LocalDate;
 
 public record UserRegisterDto(
         @NotBlank(message = "Provide an username")
+        @Size(min = 4, max = 16, message = "The username must have between 4 and 16 characters")
         String username,
         @NotBlank(message = "Provide a display name")
+        @Size(min = 4, max = 32, message = "The display name must have between 4 and 32 characters")
         String displayName,
         @NotBlank(message = "Provide an email address")
         @Email(message = "Provide a valid email")
         String email,
         @NotBlank(message = "Provide a password")
+        @Size(min = 8, message = "The password must have at least 8 characters")
         String password,
         @NotBlank(message = "Confirm the password")
+        @Size(min = 8, message = "The password must have at least 8 characters")
         String passwordConfirmation,
         LocalDate birthDate,
         String profilePicture,
