918603c99430cf48b975e3ed64e7b25598b566d7
willotavio <89710130+willotavio@users.noreply.github.com>
2024-11-23 20:41:04 -0300

    feat: add PostController/Service/Repository

---
+++ src/main/java/com/posturstuff/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.posturstuff.repository;
+
+import com.posturstuff.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
