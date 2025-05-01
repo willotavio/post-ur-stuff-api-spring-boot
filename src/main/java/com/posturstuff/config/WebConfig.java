4d230266d52dd655bda4f8478a63c6d4b7ea9dfe
willotavio <89710130+willotavio@users.noreply.github.com>
2025-05-01 12:26:36 -0300

    fix: add heroku app domain on WebConfig and SecurityConfig

--- src/main/java/com/posturstuff/config/WebConfig.java
+++ src/main/java/com/posturstuff/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://post-ur-stuff-vite-69edfb4cfb56.herokuapp.com")
                         .allowedMethods(HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
                                 HttpMethod.PATCH.name(),
